ad15b750b04acaa5791fb1a3b1968420e19bbae6
wardslm <97646097+wardslm@users.noreply.github.com>
2022-09-23 15:19:03 -0400

    Update GameEngine.h

---
+++ GameEngine.h
@@ -0,0 +1,52 @@
+//
+// Created by Warda Salem on 2022-09-22.
+//
+
+#ifndef COMP345_GAMEENGINE_H
+#define COMP345_GAMEENGINE_H
+
+#endif //COMP345_GAMEENGINE_H
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+
+#include <iostream>
+// istream and ostream are defined in the std namespace in the iostream library
+using namespace std;
+
+class GameEngine
+{
+public:
+    void startupPhase();
+};
+
+class Startup
+
+{
+public:
+    void loadmap();
+    void validateMap();
+    void addPlayer();
+
+};
+
+class Play
+{
+public:
+    void assignReinforcement();
+    void issueOrders();
+    void executeorders();
+    void endexecuteorders();
+    void win();
+};
+
+class Subs
+{
+public:
+    void printSubs(string str);
+
+};
+
+#endif
+
+
