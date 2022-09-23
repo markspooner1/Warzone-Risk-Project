722a0ebc5626489b515fbbeb39bab83d315b2d40
wardslm <97646097+wardslm@users.noreply.github.com>
2022-09-23 15:18:27 -0400

    Update GameEngineDriver.cpp

---
+++ GameEngineDriver.cpp
@@ -0,0 +1,13 @@
+#include "gameEngine.h"
+
+using namespace std;
+
+
+int main() {
+
+    // starts the startup phase then leads to the play phase once user enters gamestart
+
+    GameEngine startup;
+    startup.startupPhase();
+
+}
