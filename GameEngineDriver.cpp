0168c57dfbe5b1a9295ed438aa8e28239f1ff73e
Brahim <55364095+brahimho@users.noreply.github.com>
2022-10-09 19:12:59 -0400

    Update GameEngineDriver.cpp

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void testGameStates()
 {
 
-    // starts the startup phase then leads to the play phase once user enters gamestar
-    testGameStates();
+    // starts the startup phase then leads to the play phase once user enters gamestart
+
+    GameEngine startup;
+    startup.startupPhase();
 }
