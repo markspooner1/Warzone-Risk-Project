570ab7daedf19f182d5ecdb65dd3abce5956dcce
Brahim <55364095+brahimho@users.noreply.github.com>
2022-10-09 19:12:24 -0400

    Update GameEngine.cpp

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -368,11 +368,4 @@ void GameEngine::startupPhase()
     }
 }
 
-void testGameStates()
-{
 
-    // starts the startup phase then leads to the play phase once user enters gamestart
-
-    GameEngine startup;
-    startup.startupPhase();
-}
